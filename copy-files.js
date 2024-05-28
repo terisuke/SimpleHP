8ded557793fc6125e449327e7ef0cbdd19faa0d2
terisuke <terisuke1115@gmail.com>
2024-05-28 18:02:00 +0900

    add copy-files.js

---
+++ copy-files.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyFiles(srcDir, destDir) {
+  fs.readdirSync(srcDir).forEach(file => {
+    const srcFile = path.join(srcDir, file);
+    const destFile = path.join(destDir, file);
+
+    if (fs.lstatSync(srcFile).isDirectory()) {
+      if (file !== 'dist') { // distディレクトリを除外
+        fs.mkdirSync(destFile, { recursive: true });
+        copyFiles(srcFile, destFile);
+      }
+    } else {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+copyFiles('.', './dist');
